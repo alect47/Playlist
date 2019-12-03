41e968bf0af82e0fe4d9f6f468162e2f3712fd3a
Alec Wells <alec.wells47@gmail.com>
2019-12-03 10:57:24 -0700

    Add favorite class with constructor and getGenre function
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

---
+++ models/favorite.js
@@ -0,0 +1,26 @@
+class Favorite {
+
+  constructor(musicData, genreData) {
+    this.title = musicData.track_name;
+    this.artistName = musicData.artist_name;
+    // console.log(setGenre(genreData));
+    this.genre = genreData;
+    // this.genre = genreData;
+    this.rating = musicData.track_rating;
+  }
+
+// This function isn't working, but it could be useful, otherwise need to set a default value
+// in the database
+// Might need to make it an async
+  setGenre(genreData) {
+    if(!genreData) {
+      this.genre = "Unknown"
+    }
+    else {
+      this.genre = genreData.music_genre.music_genre_name
+    }
+  }
+
+}
+
+module.exports = Favorite;
