f8ad15970d9c1018f90dfcd7694afab2a4ab487d
Alec Wells <alec.wells47@gmail.com>
2019-12-03 10:58:22 -0700

    Create instance of favorite in musicService
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- services/musixService.js
+++ services/musixService.js
@@ -2,13 +2,17 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require('node-fetch');
-// const Favorite = require('../models/favorite')
+const Favorite = require('../models/favorite')
 
 async function fetchMusic(title, artist) {
   let response = await fetch(`http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&s_track_rating=desc&apikey=${process.env.MUSIXMATCH_API_KEY}&q_track=${title}`);
   let songs = await response.json();
-  // console.log(songs)
-  return songs;
+  let musicData = await songs.message.body.track_list[0].track
+  let genreData = await musicData.primary_genres.music_genre_list[0]
+  let formattedFavorite = await new Favorite(musicData, genreData)
+  await formattedFavorite.setGenre(genreData)
+  console.log(formattedFavorite)
+  return formattedFavorite;
 }
 
 module.exports = {
