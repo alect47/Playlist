3248649a5f6a71dd93deadd8402098c17f9c145a
Alec Wells <alec.wells47@gmail.com>
2019-12-03 13:39:19 -0700

    Add conditional to fetMusic service to handle bad requests
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- services/musixService.js
+++ services/musixService.js
@@ -5,14 +5,15 @@ const fetch = require('node-fetch');
 const Favorite = require('../models/favorite')
 
 async function fetchMusic(title, artist) {
-  let response = await fetch(`http://api.musixmatch.com/ws/1.1/track.search?q_artist=${artist}&s_track_rating=desc&apikey=${process.env.MUSIXMATCH_API_KEY}&q_track=${title}`);
+  let response = await fetch(`http://api.musixmatch.com/ws/1.1/matcher.track.get?q_artist=${artist}&apikey=${process.env.MUSIXMATCH_API_KEY}&q_track=${title}`);
   let songs = await response.json();
-  let musicData = await songs.message.body.track_list[0].track
-  let genreData = await musicData.primary_genres.music_genre_list[0]
-  let formattedFavorite = await new Favorite(musicData, genreData)
-  await formattedFavorite.setGenre(genreData)
-  console.log(formattedFavorite)
-  return formattedFavorite;
+  if(songs.message.body) {
+    let musicData = await songs.message.body.track
+    let genreData = await musicData.primary_genres.music_genre_list[0]
+    let formattedFavorite = await new Favorite(musicData, genreData)
+    await formattedFavorite.setGenre(genreData)
+    return formattedFavorite;
+  }
 }
 
 module.exports = {
