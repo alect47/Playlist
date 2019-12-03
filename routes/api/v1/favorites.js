83ce6e2846735da41e22e5bf3574c199037eb7c0
Alec Wells <alec.wells47@gmail.com>
2019-12-03 13:38:20 -0700

    Finish post in favorites router
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -1,5 +1,4 @@
 var express = require('express');
-// var favorite = require('./favorite');
 var router = express.Router();
 
 const environment = process.env.NODE_ENV || 'development';
@@ -14,34 +13,20 @@ router.post('/', (request, response) => {
   const song = request.body
   var title = song.song_title
   var artist = song.artist
+
   fetchMusic(title, artist)
     .then(musicData => {
-      // this is what we want to feed to the constructor to make the hash
-      console.log(musicData)
-      // all tracks have a music_genre_list, but some are empty arrays, need some logic to acoount for this
-      // probably in model for formatting
-      // The line below is what we will give to he setGenre function in the favorite class
-      // console.log(musicData.message.body.track_list[0].track.primary_genres.music_genre_list[0])
+      if(musicData) {
+      database('favorites').insert({ title: `${musicData.title}`, artistName: `${musicData.artistName}`, genre: `${musicData.genre}`, rating: `${musicData.rating}`}, "id")
+        .then(id => {
+          musicData.id = id[0]
+        })
+          .then(data => response.status(201).send(musicData))
+        }
+        else {
+          response.sendStatus(400)
+        }
     })
-  database('favorites').insert({ title: `${song}`}, "id")
-        .then(data => response.status(201).send(`"message": "${song} has been added to your favorites"`))
-        .catch(reason => response.send(reason.message))
-
-  // database('users').where('api_key', request.body.api_key)
-  //   .then(users => {
-  //     if (users.length) {
-  //       database('favorites').insert({ location: `${location}`, user_id: `${users[0].id}`}, "id")
-  //         .then(data => response.status(201).send(`"message": "${location} has been added to your favorites"`))
-  //         .catch(reason => response.send(reason.message))
-  //     } else {
-  //       response.status(401).json({
-  //         error: `Unauthorized, Could not find user with api key: ${request.body.api_key}`
-  //       });
-  //     }
-  //   })
-  //   .catch(error => {
-  //     response.status(500).json({ error });
-  //   });
 });
 
 module.exports = router;
