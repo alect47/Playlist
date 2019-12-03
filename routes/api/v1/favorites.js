c801d72dc5fd9513f33ccdd536378ed635157aec
Alec Wells <alec.wells47@gmail.com>
2019-12-03 10:57:55 -0700

    Add post router to favorites
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- routes/api/v1/favorites.js
+++ routes/api/v1/favorites.js
@@ -7,8 +7,8 @@ const configuration = require('../../../knexfile')[environment];
 const database = require('knex')(configuration);
 const fetch = require('node-fetch');
 
-const helpers = require('../../../services/musixService');
-const fetchMusic = helpers.fetchMusic;
+const musixService = require('../../../services/musixService');
+const fetchMusic = musixService.fetchMusic;
 
 router.post('/', (request, response) => {
   const song = request.body
@@ -16,7 +16,12 @@ router.post('/', (request, response) => {
   var artist = song.artist
   fetchMusic(title, artist)
     .then(musicData => {
+      // this is what we want to feed to the constructor to make the hash
       console.log(musicData)
+      // all tracks have a music_genre_list, but some are empty arrays, need some logic to acoount for this
+      // probably in model for formatting
+      // The line below is what we will give to he setGenre function in the favorite class
+      // console.log(musicData.message.body.track_list[0].track.primary_genres.music_genre_list[0])
     })
   database('favorites').insert({ title: `${song}`}, "id")
         .then(data => response.status(201).send(`"message": "${song} has been added to your favorites"`))
