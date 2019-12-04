c594959ed568a14dfc543c55bfb1251949b4e650
Alec Wells <alec.wells47@gmail.com>
2019-12-04 12:51:14 -0700

    Remove staging from knexfile.js
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -23,20 +23,7 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: `${process.env.PRODUCTION_DB}`,
-    // connection: 'postgres://mztouxgmsntjsh:5908998545484fb1a334ac1123311ecbded76f60212e6fe658de5fa154e3ce4c@ec2-174-129-255-76.compute-1.amazonaws.com:5432/d29t0b8jgphgn',
-    migrations: {
-      directory: './db/migrations'
-    },
-    seeds: {
-      directory: './db/seeds/dev'
-    },
-    useNullAsDefault: true
-  },
-  staging: {
-    client: 'pg',
-    connection: `${process.env.STAGING_DB}`,
-    // connection: 'postgres://jbtgbgdwjucbaj:6200234cfea52892ab933ebd5d4de153976f8d0e758ec762211a921fb9718d67@ec2-174-129-255-46.compute-1.amazonaws.com:5432/d469hfan6hj3ut',
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: './db/migrations'
     },
