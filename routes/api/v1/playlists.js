19a954c55c5608f305d2dbb5bf02b14cb86db83f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-12-06 12:49:59 -0700

    Refactors to no longer use models/playlist.js.
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-const Playlist = require('../../../models/playlist')
 
 
 const environment = process.env.NODE_ENV || 'development';
