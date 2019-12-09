b218b5fd9d9f77edf4e8d19547db938d1434e39f
Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>
2019-12-09 12:56:15 -0700

    Adds new migration to create join table playlist_favorites
    Co-authored-by: Alec Wells <23268508+alect47@users.noreply.github.com>

--- db/migrations/20191208134808_addPlaylistIDtoFavorites.js
+++ db/migrations/20191208134808_addPlaylistIDtoFavorites.js
@@ -1,17 +1,19 @@
-// exports.up = function(knex) {
-//   return Promise.all([
-//     knex.schema.alterTable('favorites', function(table) {
-//       table.integer('playlist_id').unsigned()
-//       table.foreign('playlist_id')
-//         .references('playlists.id');
-//     })
-//   ])
-// };
-//
-// exports.down = function(knex) {
-//   return Promise.all([
-//     knex.schema.alterTable('favorites', function(table){
-//       table.dropColumn('playlist_id');
-//     })
-//   ])
-// };
+exports.up = function(knex) {
+  return Promise.all([
+    knex.schema.createTable('playlist_favorites', function(table) {
+      table.increments('id').primary();
+      table.integer('playlist_id').unsigned()
+      table.foreign('playlist_id')
+        .references('playlists.id');
+      table.integer('favorites_id').unsigned()
+      table.foreign('favorites_id')
+        .references('favorites.id');
+    })
+  ])
+};
+
+exports.down = function(knex) {
+  return Promise.all([
+    knex.schema.dropTable('playlist_favorites')
+  ])
+};
