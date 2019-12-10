920a987267219e38c8e92d35dd8801390671c98d
Alec Wells <alec.wells47@gmail.com>
2019-12-10 14:34:13 -0700

    Add getFavorite and addFavorite functions to playlist model
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- models/playlist.js
+++ models/playlist.js
@@ -1,3 +1,7 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
 class Playlist {
 
   constructor(playlistData, favoriteData) {
@@ -5,12 +9,26 @@ class Playlist {
     this.title = playlistData.title;
     this.songCount = 0;
     this.songAvgRating = 0;
-    this.favorites = [];
+    this.favorites = [] ;
   }
 
-  addFavorite(favoriteData) {
-    this.favorites.push(favoriteData)
+ async getFavorite(favoriteId) {
+    let data = database('favorites').where('id', favoriteId).first()
+    return data
   }
+
+  async addFavorite(favoriteData) {
+    const promises = await favoriteData.map(async (favorite) => {
+      let favoriteInfo = await this.getFavorite(favorite.favorites_id)
+      return favoriteInfo
+  });
+  return Promise.all(promises).then(favorites => {
+    favorites.forEach(favorite => {
+      this.favorites.push(favorite)
+    })
+  });
+}
+
 
 }
 
