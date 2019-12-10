1dcf0d67290371505b0396ec8a096de74378db42
Alec Wells <alec.wells47@gmail.com>
2019-12-10 16:06:07 -0700

    Add getAllFavorites to playlist model
    Co-authored-by: Smitha Hosmani <32909448+hsmitha26@users.noreply.github.com>

--- models/playlist.js
+++ models/playlist.js
@@ -4,17 +4,32 @@ const database = require('knex')(configuration);
 
 class Playlist {
 
-  constructor(playlistData, favoriteData) {
+  constructor(playlistData) {
     this.id = playlistData.id;
     this.title = playlistData.title;
     this.songCount = 0;
     this.songAvgRating = 0;
-    this.favorites = [] ;
+    this.favorites;
+    this.created_at = playlistData.created_at;
+    this.updated_at = playlistData.updated_at;
   }
 
  async getFavorite(favoriteId) {
     let data = database('favorites').where('id', favoriteId).first()
     return data
+  }
+
+ async getAllFavorites(playlistId) {
+
+    try {
+      let response = await database('playlist_favorites')
+                .select('favorites.id', 'favorites.title', 'favorites.artistName', 'favorites.genre', 'favorites.rating')
+                .join('favorites', {'favorites.id': 'playlist_favorites.favorites_id'})
+                .where('playlist_favorites.playlist_id', playlistId)
+      this.favorites = response
+    } catch(err) {
+      return err;
+    }
   }
 
   async addFavorite(favoriteData) {
@@ -32,6 +47,20 @@ class Playlist {
   async totalSongs() {
      this.songCount = await this.favorites.length
    }
+
+  async totalRating() {
+     let rating = 0
+     this.favorites.forEach(favorite => {
+       rating += favorite.rating
+     })
+     return rating
+   }
+
+   async averageRating() {
+      let count = await this.songCount
+      let totalRating = await this.totalRating()
+      this.songAvgRating = (totalRating/count)
+    }
 
 }
 
